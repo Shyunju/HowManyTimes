cb7118b7beeb22ba4175ab4a34624fac756ba536
Shyunju <hyunju9908@naver.com>
2026-02-05 18:49:25 +0900

    add true ending scene

---
+++ Assets/Scripts/GoToTrueEnding.cs
@@ -0,0 +1,13 @@
+using UGESystem;
+using UnityEngine.SceneManagement;
+
+public class GoToTrueEnding : AbstractEventReward
+{
+    public override void GrantReward(UGEEventTaskRunner runner)
+    {
+        if(GameManager.Instance.Likeability > 60)
+        {
+            SceneManager.LoadScene("TrueEnding");
+        }
+    }
+}
